5aeba35888f88ed60f586fa4e0b10f17c3f20c13
Martis467 <34279764+Martis467@users.noreply.github.com>
2019-03-06 12:29:45 +0200

    Update app.py

--- Pirmas/app.py
+++ Pirmas/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, g
 from flask_restful import Api
 import markdown
+impot shelve
 
 from User import UserList, User
 
@@ -18,6 +19,87 @@ def index():
 
         return markdown.markdown(content)
 
+    
+class UserList(Resource):
+    def get(self):
+        shelf = get_db()
+        keys = list(shelf.keys())
+
+        users = []
+
+        for key in keys:
+            users.append(shelf[key])
+
+        return {'message': 'Success', 'data': users}, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('firstName', required=True)
+        parser.add_argument('lastName', required=True)
+        parser.add_argument('email', required=True)
+
+        # Parser arguments into obj
+        args = parser.parse_args()
+
+        shelf = get_db()
+        shelf[args['email']] = args
+
+        return {'message': 'User created', 'data': args}, 201
+
+
+class User(Resource):
+    def get(self, email):
+        shelf = get_db()
+
+        if not (email in shelf):
+            return {'message': 'User not found', 'data': {}}, 404
+
+        return {'message': 'User', 'data': shelf[email]}, 200
+
+    def put(self, email):
+        shelf = get_db()
+
+        if not (email in shelf):
+            return {'message': 'User not found', 'data': {}}, 404
+
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('firstName', required=True)
+        parser.add_argument('lastName', required=True)
+        parser.add_argument('email', required=True)
+
+        # Parser arguments into obj
+        args = parser.parse_args()
+
+        shelf = get_db()
+        shelf[email] = args
+
+        return {'message': 'User updated successfully', 'data': args}, 202
+
+    def delete(self, email):
+        shelf = get_db()
+
+        if not (email in shelf):
+            return {'message': 'User not found', 'data': {}}, 404
+
+        del shelf[email]
+
+        return '', 204
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = shelve.open("users.db")
+    return db
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 api.add_resource(UserList, '/users')
 api.add_resource(User, '/users/<string:email>')
