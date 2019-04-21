1320b6b1678313b205e697b2e5cd2e4996ba5041
DESKTOP-712L91G\Martis <martynas.jakimcikas@gmail.com>
2019-04-21 15:13:45 +0300

    Fixing models

--- Antras/models/NoteModel.py
+++ Antras/models/NoteModel.py
@@ -7,7 +7,14 @@ class Note:
         self.expiration = obj['expiration']
 
     def serialize(self):
+        index = 0
+
+        try:
+            index = self.title.index(self.separationIndex)
+        except ValueError as e:
+            index = len(self.title)
+
         return {
-            'title': self.title[self.title.index(self.separationIndex)],
+            'title': self.title[0:index],
             'comment': self.comment,
         }
